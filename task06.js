6f726833cd0522ebbb1179391e98700c1cc2177d
teenov25 <56924764+teenov25@users.noreply.github.com>
2022-03-24 18:47:28 +0200

    changed declaration on line number 4

--- task06.js
+++ task06.js
@@ -1,13 +1,12 @@
-function findMaximumNumber (){
+function findMaximumNumber() {
     let maximumNumber = arguments[0];
 
-    for (var i = 0; i < arguments.length;  i++){
-        if (arguments[i] > maximumNumber ){
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] > maximumNumber) {
             maximumNumber = arguments[i];
-        } 
+        }
     }
     return maximumNumber;
-} 
+}
 
-console.log(findMaximumNumber(12,17,3,5,9,90,67))
-    
+console.log(findMaximumNumber(12, 17, 3, 5, 9, 90, 67))
